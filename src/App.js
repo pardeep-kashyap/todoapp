7f2945daf7527f524b1c954491fe052260b64f6c
pardeep <pardeep.kashyap@tothenew.com>
2020-09-29 22:08:24 +0530

    HAsh Router'

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './App.scss';
 import { EntryComponent } from './components/entryComponent/entryComponent.js';
-import { Route, BrowserRouter as Router } from 'react-router-dom';
+import { Route, BrowserRouter as Router,HashRouter } from 'react-router-dom';
 import { HomeComponent } from "./components/homeComponent/homeComponent.js";
 import SignUpComponent from './components/signUpComponent/signUpComponent';
 
 export default class App extends React.Component{
     render (){
         return (
-            <Router>
+            <HashRouter>
                 <Route exact path="/" component={EntryComponent} />
                 <Route path="/home" component={HomeComponent} />
                 <Route path="/new-user" component={SignUpComponent} />
-            </Router>)
+            </HashRouter>)
     }
 };
